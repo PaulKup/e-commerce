9b1ce14a168ce9039cc76b3f31974ddb32ef5241
ChillyWilly <noreply>
2021-03-31 05:20:37 -0400

    api routes complete

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
